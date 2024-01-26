0a41672111439507504c4d7a8d6d7492e987c479
Elmalik07 <malik.tiemogo@gmail.com>
2024-01-26 14:12:04 +0000

    hello Darryn

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React - F2F - 
+          Learn React - F2F - Darryn
         </a>
       </header>
     </div>
