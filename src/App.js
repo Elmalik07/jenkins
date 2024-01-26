23176f2dacb337ce054c10b8210edca2323100b9
Elmalik07 <malik.tiemogo@gmail.com>
2024-01-26 13:58:38 +0000

    third commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React - F2F
+          Learn React - F2F - 
         </a>
       </header>
     </div>
